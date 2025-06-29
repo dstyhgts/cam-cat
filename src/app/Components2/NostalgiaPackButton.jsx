fb9811eba592576b1b67d91337b799f7592408df
dstyhgts <94107821+dstyhgts@users.noreply.github.com>
2025-06-29 14:53:47 -0700

    testimonial

--- src/app/Components2/NostalgiaPackButton.jsx
+++ src/app/Components2/NostalgiaPackButton.jsx
@@ -59,7 +59,7 @@ const NostalgiaPackButton = () => {
         <div className="printpack-card printpack-default">
             <p className="printpack-topline">Learn about our...</p>
           <h2 className="printpack-title">
-            <span className="nostalgia-title-word"><em>NOSTALGIA</em></span><br />
+            <span className="nostalgia-title-word"><em>MEMORY</em></span><br />
             <span className="package-title-word">PACKAGE</span>
           </h2>
           <p className="printpack-subtext">
@@ -82,8 +82,8 @@ const NostalgiaPackButton = () => {
           <div className="printpack-expanded-card left-card">
             {/* Overhanging title box */}
             <div className="printpack-overflow-title">
-              <h3>NOSTALGIA</h3>
-              <p>Indisposible Memories.</p>
+              <h3>MEMORIES...</h3>
+              <p>Indisposible Nostalgia.</p>
             </div>
 
             {/* Inner frame for text content */}
