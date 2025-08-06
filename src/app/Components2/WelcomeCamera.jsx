0bed5a79345864dc6cf557783f64887a2a3682d5
dstyhgts <94107821+dstyhgts@users.noreply.github.com>
2025-08-05 19:48:57 -0700

    Update WelcomeCamera.jsx

--- src/app/Components2/WelcomeCamera.jsx
+++ src/app/Components2/WelcomeCamera.jsx
@@ -53,12 +53,12 @@ const WelcomeCamera = () => {
         <div className="left-content" style={{ pointerEvents: 'none' }}>
           {/* <h1 className="main-title">YOU HOLD THE CAMERA*</h1> */}
           {/* <h1 className="main-title">UNLIMITED CONTENT AT YOUR EVENT.</h1> */}
-          <h1 className="main-title" style={{ marginTop: '15px' }}>WE BRING THE CAMERAS.</h1>
+          <h1 className="main-title" style={{ marginTop: '15px' }}>WE BRING THE <i>DIGI- CAMS!</i></h1>
           {/* <p className="subtitle">
             Every celebration is a story. Camera Catering makes each guest the storyteller.
           </p> */}
           <p className="subtitle" style={{ marginTop: '24px' }}>
-            DITCH THE DISPOSABLES, FORGET THE PHOTO BOOTH, AND GO DIGITAL.
+            DITCH THE DISPOSABLES, FORGET THE PHOTO BOOTH... USE YOUR HANDS, AND <i>GO DIGITAL</i>.
           </p>
 
           <div className="new-section">
@@ -66,7 +66,7 @@ const WelcomeCamera = () => {
             <p className="new-description">
               {/* Rapid® Print cameras to take the user experience to the next level. 
               It's basically a hand-held Photo Booth, but so much better. Trust us. */}
-              <i>Your guests night will be remembered by them. We'll make sure of it.</i>
+              <i>Your event will be remmebered by every guest... by their own hand.</i>
             </p>
             <p className="new-description">
               <i>With the possibility for thousands of photographs and videos of your event, 
