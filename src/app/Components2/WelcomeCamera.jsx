4cde7a6fe796664ecf61dee8f7eff1d8317573b9
dstyhgts <94107821+dstyhgts@users.noreply.github.com>
2025-08-15 16:47:15 -0700

    Update WelcomeCamera.jsx

--- src/app/Components2/WelcomeCamera.jsx
+++ src/app/Components2/WelcomeCamera.jsx
@@ -58,7 +58,7 @@ const WelcomeCamera = () => {
             Every celebration is a story. Camera Catering makes each guest the storyteller.
           </p> */}
           <p className="subtitle" style={{ marginTop: '24px' }}>
-            <i>DITCH</i> THE DISPOSABLES, GET <i>NOSTALGIC,</i> AND <i><u>GO VINTAGE!</u></i>
+            <i>DITCH</i> THE DISPOSABLES, GET <i>NOSTALGIC,</i> AND <i>GO <u>DIGITAL!</u></i>
           </p>
 
           <div className="new-section">
